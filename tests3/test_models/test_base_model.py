b445a1cee2a0f85ef6d36e3871850b19d38cc8cf
marianellamonroyortizhb <1880@holbertonschool.com>
2021-02-18 03:55:22 +0000

    Added: Test files

---
+++ tests3/test_models/test_base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Test Case Base Model"""
+
+
+import re
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+from time import sleep
+
+
+class TestBaseModel(unittest.TestCase):
+    """Test Base Model"""
